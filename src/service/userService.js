274d1d8e7c0b084c7ed573877965b06e0a511532
Natalia443 <96155586+Natalia443@users.noreply.github.com>
2024-06-20 19:42:49 -0300

    cookies modificadas

--- src/service/userService.js
+++ src/service/userService.js
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 
 const client = axios.create({
   baseURL: "https://rivalearn-backend.onrender.com/api/",
-  /*baseURL: "http://localhost:3001/api/",*/
+  //baseURL: "http://localhost:3001/api/",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
@@ -13,7 +13,11 @@ const client = axios.create({
 export default {
   async saveUser(username, password, email) {
     try {
-      const user = await client.post("users/signup", { username, password, email });
+      const user = await client.post("users/signup", {
+        username,
+        password,
+        email,
+      });
       return user;
     } catch (error) {
       throw "Error al guardar el usuario";
@@ -26,14 +30,17 @@ export default {
         username: usernameInput,
         password: passwordInput,
       });
-      const { access_token, refresh_token, username, user_id } = response.data;
+      const { access_token, refresh_token, expiry_date, username, user_id } =
+        response.data;
+      const expiryDate = new Date(expiry_date);
       Cookies.set("access_token", access_token, { expires: 1 / 24 });
-      Cookies.set("refresh_token", refresh_token, { expires: 7 });
+      Cookies.set("refresh_token", refresh_token, { expires: expiryDate });
+      console.log(expiryDate);
       Cookies.set("username", username);
       Cookies.set("user_id", user_id);
       return { username: username };
     } catch (error) {
-      throw "Error al iniciar sesion";
+      throw error.message;
     }
   },
 
@@ -68,74 +75,71 @@ export default {
 
   async getDeckFlashCards(deckId) {
     try {
-        if (!deckId) {
-            throw new Error("No se encontró el deckId");
-        }
-        
-        const response = await client.get(`flashcards/get/${deckId}`);
-        
-        if (response.status !== 200) {
-            throw new Error("Error en la solicitud de flashcards");
-        }
+      if (!deckId) {
+        throw new Error("No se encontró el deckId");
+      }
 
-        return response.data;
+      const response = await client.get(`flashcards/get/${deckId}`);
+
+      if (response.status !== 200) {
+        throw new Error("Error en la solicitud de flashcards");
+      }
+
+      return response.data;
     } catch (error) {
-        throw new Error(`Error al obtener los flashcards: ${error.message}`);
+      throw new Error(`Error al obtener los flashcards: ${error.message}`);
     }
-},
+  },
 
-async createFlashcard(deckId, palabra, idioma, traduccion) {
-  try {
-    const response = await client.post(`flashcards/create`, {
-      deckId: deckId,
-      vocab: palabra,
-      sourceLang: idioma,
-      targetLang: traduccion,
-    });
-    return response.data;
-  } catch (error) {
-    throw "Error al crear la flashcard";
-  }
-}, 
+  async createFlashcard(deckId, palabra, idioma, traduccion) {
+    try {
+      const response = await client.post(`flashcards/create`, {
+        deckId: deckId,
+        vocab: palabra,
+        sourceLang: idioma,
+        targetLang: traduccion,
+      });
+      return response.data;
+    } catch (error) {
+      throw "Error al crear la flashcard";
+    }
+  },
 
+  async getFlashcardById(flashcardId) {
+    try {
+      const response = await client.get(`flashcards/detail/${flashcardId}`);
+      return response.data;
+    } catch (error) {
+      throw new Error(
+        `Error al obtener los detalles de la flashcard: ${error.message}`
+      );
+    }
+  },
 
-async getFlashcardById(flashcardId) {
-  try {
-    const response = await client.get(`flashcards/detail/${flashcardId}`);
-    return response.data;
-  } catch (error) {
-    throw new Error(`Error al obtener los detalles de la flashcard: ${error.message}`);
-  }
-},
+  async createStory(words) {
+    try {
+      const response = await client.post("gemini/story", { words });
+      return response.data;
+    } catch (error) {
+      throw new Error(`Error al crear la historia: ${error.message}`);
+    }
+  },
 
+  async deleteFlashcard(flashcardId) {
+    try {
+      const response = await client.delete(`flashcards/delete/${flashcardId}`);
+      return response.data;
+    } catch (error) {
+      throw new Error(`Error al borrar la Flashcard: ${error.message}`);
+    }
+  },
 
-async createStory(words) {
-  try {
-    const response = await client.post("gemini/story", { words });
-    return response.data;
-  } catch (error) {
-    throw new Error(`Error al crear la historia: ${error.message}`);
-  }
-},
-
-async deleteFlashcard(flashcardId) {
-  try {
-    const response = await client.delete(`flashcards/delete/${flashcardId}`);
-    return response.data;
-  } catch (error) {
-    throw new Error(`Error al borrar la Flashcard: ${error.message}`);
-  }
-},
-
-async deleteDeck(deckId) {
-  try {
-    const response = await client.delete(`decks/delete/${deckId}`);
-    return response.data;
-  } catch (error) {
-    throw new Error(`Error al borrar la Flashcard: ${error.message}`);
-  }
-}
-
-
+  async deleteDeck(deckId) {
+    try {
+      const response = await client.delete(`decks/delete/${deckId}`);
+      return response.data;
+    } catch (error) {
+      throw new Error(`Error al borrar la Flashcard: ${error.message}`);
+    }
+  },
 };
-
